335ebd8767958113c92c78350a22d3467f22d195
WeldonThallysson <65944963+WeldonThallysson@users.noreply.github.com>
2022-05-25 23:06:44 -0300

    Create Api.js

---
+++ Api.js
@@ -0,0 +1,58 @@
+# TesteCarrinho_Comprasimport React from 'react'
+
+
+ const Api = [
+       
+     {
+        id: "16701615",
+        name: "Trufa BENDITO CACAU 55% CACAU 30 G",
+        price: 3.03,
+        imageUrl: "http://codeby.vteximg.com.br/arquivos/ids/159959-800-1029/truffon-meio-amargo.png?v=636930938547630000",
+         
+     },
+     {
+        id: "16701612",
+        name: "TRUFA MORANGO",
+        price: 1.23,
+        imageUrl: "http://codeby.vteximg.com.br/arquivos/ids/159939-800-1029/trufa-morango-30g.png?v=636916431597070000",
+
+     },
+     {
+        id: "16701610" ,
+        name: "TRUFA TRADICIONAL 30 G",
+        price: 3.03,
+        imageUrl: "http://codeby.vteximg.com.br/arquivos/ids/159942-800-1029/trufa-tradicional.png?v=636916452526400000",
+     },
+     {
+        id: "16701618", 
+        name: "TRUFA DE COCO 30 G", 
+        price: 3.03,
+        imageUrl: "http://codeby.vteximg.com.br/arquivos/ids/159957-800-1029/trufa-coco.png?v=636929369885800000"
+     },
+     {
+        id: "16701609",
+        name: "TRUFA BRANCO 30G",
+        price: 1.10,
+        imageUrl: "http://codeby.vteximg.com.br/arquivos/ids/159993-800-1029/trufa-branco.png?v=636945743710500000"
+     },
+     {
+        id: "16701611",
+        name: "TRUFA MEZZO 30 G",
+        price: 3.03,
+        imageUrl: "http://codeby.vteximg.com.br/arquivos/ids/159941-800-1029/mezzo.png?v=636916443729870000",
+        "detailUrl": "/trufa-mezzo/p",
+     },
+     {
+        id: "16701608",
+        name: "TRUFA MORANGO E MARACUJÁ 13,5 G",
+        price: 1.10,
+        imageUrl: "http://codeby.vteximg.com.br/arquivos/ids/159936-800-1029/Trufa-Morango-e-Maracuja.png?v=636915614811230000",
+     },
+   
+
+
+
+
+]
+
+export default Api;
